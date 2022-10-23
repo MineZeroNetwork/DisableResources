17e87e24b8c5fccd210dd7304b7bfea1522f7b34
chiharu <chiharuhagihara803@gmail.com>
2022-10-23 10:58:35 +0900

    initial commit

---
+++ src/main/java/net/minezero/disableresources/DisableResources.java
@@ -0,0 +1,17 @@
+package net.minezero.disableresources;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class DisableResources extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
